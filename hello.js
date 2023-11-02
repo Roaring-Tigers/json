35f1c8d02897a966a4564bd83b18e53671c3af36
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-02 11:02:57 +0530

    json

---
+++ hello.js
@@ -0,0 +1,47 @@
+
+// const obj = {
+//     a:10,
+//     b:20
+// }
+
+// JSON 
+
+// JSOn is just a key value lnagugae: 
+
+// objJson = {
+//     "a" : 10,
+//     "b" : 20
+// }
+
+// Stringify:
+
+// console.log(obj)
+// console.log(JSON.stringify(obj))
+
+// let objJson = JSON.stringify(obj)
+
+// console.log(objJson, typeof objJson)
+
+
+// let arr = [10,'apple',true,undefined,null, [1,2,3,4,5], {a:10,b:20}]
+
+// console.log(arr)
+
+// let arrJson = JSON.stringify(arr)
+
+// console.log(arrJson, typeof arrJson)
+
+// let a = 10
+
+
+// json data types: 
+
+// number , boolean, string(""), array, object(""  in string), null
+
+
+
+//parse: 
+// let jsonstr = '[10,"apple",true,null,null,[1,2,3,4,5],{"a":10,"b":20}]'
+// let arr = JSON.parse(jsonstr) 
+
+// console.log(arr, typeof arr)
